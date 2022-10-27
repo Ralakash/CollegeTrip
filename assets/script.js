1ee994dcb7370925101b46fb4117c4f0e3fd9440
Ralakash <shawnkaralash@gmail.com>
2022-10-26 22:04:12 -0400

    Please dont merge

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,88 @@
-var collegeName = "Kalamazoo";
-var saveBtn = $("#saveBtn");
-var openweathermapKey = "99f3a7e5c08aca58214a5ff5a0495fd1";
-const scorecardAPI = "DoaHn0fsthkrMipg5cL4hkdLpAWcI407Je2idkJN";
-var schoolinfo;
-var weather1;
-var city = "kalamazoo";
 
-saveBtn.on('click', function () {
+
+const schoolsWithWeather = [];
+var schoolInfo;
+var resultsInfo;
+
+// obtain schools
+const collegescorecard = (collegeName) =>{
+    console.log(collegeName);
     // debugger;
- collegeName = $("#collegeName").val();
- collegescorecard();
-})
+    const scorecardAPI = "DoaHn0fsthkrMipg5cL4hkdLpAWcI407Je2idkJN";
+    fetch("https://api.data.gov/ed/collegescorecard/v1/schools?school.name=" + collegeName + "&api_key=DoaHn0fsthkrMipg5cL4hkdLpAWcI407Je2idkJN")
+    .then(res => res.json())
+    .then(data => collegescorecardReturn(data))
+}
 
-// fetch("http://universities.hipolabs.com/search?country=United%20States&name=" + collegeName)
-// .then(res => res.json())
-// .then(data => console.log(data));\
-const collegescorecard = () =>{
-fetch("https://api.data.gov/ed/collegescorecard/v1/schools?school.name=" + collegeName + "&api_key=DoaHn0fsthkrMipg5cL4hkdLpAWcI407Je2idkJN")
-.then(res => res.json())
-.then(data => collegescorecardReturn(data))
-
-    const collegescorecardReturn = (data) => {
-        var dataResults = data.results;
-        console.log(dataResults);
-        for (let i = 0; i < dataResults.length; i++) {
-            console.log(data.results[i].school.city);
-            city = dataResults[i].school.city;
-            weather();
-            
-        }
+const collegescorecardReturn = (data) => {
+    console.log(data);
+    // debugger;
+    resultsInfo = data.results;
+}
+const collegeLoop = () => {
+    for (let i = 0; i < resultsInfo.length; i++) {
+        schoolInfo = resultsInfo[i].school;
+        schoolsWithWeather[i] = schoolInfo;        
     }
-
 }
 
-
-const weather = () => {
-fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + openweathermapKey)
-.then(res => res.json())
-.then(data => {
-    const {list} = data;
-    weather1 = list[4];
-    
-    return console.log(weather1);
-});
+// obtain weather
+const callWeather = () => {
+    for (let i = 0; i < schoolsWithWeather.length; i++) {
+        console.log("callWeather" + i);
+        // debugger;
+        weather(schoolsWithWeather[i].city, i);        
+    }
 }
+
+const weather = (city,i) => {
+    console.log(city + "weather" + i);
+    // debugger;
+    const openweathermapKey = "99f3a7e5c08aca58214a5ff5a0495fd1";
+     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + openweathermapKey)
+    .then(res => res.json())
+    .then(data => weatherReturn(data,i));
+}
+
+const weatherReturn = (data,i) => {
+    console.log(data + "weatherReturn" +i);  
+    // debugger;
+    var weather1 = data.list[4].main.temp;
+    var weather2 = (weather1 -273.15) * 9/5 + 32;
+    weather2 = Math.round(weather2);
+    // pass weather into array
+    const newSchool = {...schoolInfo , currentWeather : weather2};
+    schoolsWithWeather[i] = newSchool;    
+}
+
+// print to screen
+const creatCards = () => {
+    for (let i = 0; i < schoolsWithWeather.length; i++) {
+        console.log("createCards" + i);
+        // debugger;
+        var cardID = [];
+        var cardBodyID = [];
+        cardID[i] = "#card"+i; 
+        cardBodyID[i] ="#card-body"+i;    
+        cardID = "#card"+i; 
+        cardBodyID[i] ="#card-body"+i;        
+        $("#containerEL").append('<div class="card" id="card' + i + '"></div>');
+        $(cardID[i]).append('<div class="card-body" id="card-body' + i + '"></div>');
+        console.log(cardBodyID[i]);
+        $(cardBodyID[i]).text("College name: " + schoolsWithWeather[i].name + ", City: " + schoolsWithWeather[i].city + ", School URL: " + schoolsWithWeather[i].school_url + ", Temperature(F): " +schoolsWithWeather[i].currentWeather);
+        console.log(i);
+    };
+}
+
+// button click
+$("#saveBtn").on('click', function (event) {
+    // debugger;
+    event.preventDefault();
+    $("#containerEL").empty();
+    var collegeName = $("#collegeName").val();
+    console.log(collegeName);
+    collegescorecard(collegeName);
+    collegeLoop();
+    callWeather();
+    creatCards();
+})
